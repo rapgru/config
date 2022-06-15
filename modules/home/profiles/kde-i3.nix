02b876c19eef7ee8af1330cf1f29e5398492b47e
Raphael Gruber <raphael.gruber@cloudflight.io>
2022-06-15 18:07:40 +0200

    fix: syntax

--- modules/home/profiles/kde-i3.nix
+++ modules/home/profiles/kde-i3.nix
@@ -5,19 +5,18 @@
       gaps = {
         outer = 4;
       };
-      windows = {
+      window = {
         commands = [
-          { command = "kill, floating enable, border none"; criteria = {title = "Desktop - Plasma"}; }
-          { command = "floating enable"; criteria = {class = "plasmashell"}; }
-          { command = "floating enable, border none"; criteria = {class = "Plasma"}; }
-          { command = "floating enable, border none"; criteria = {title = "plasma-desktop"}; }
-          { command = "floating enable, border none"; criteria = {title = "win7"}; }
-          { command = "floating enable, border none"; criteria = {class = "krunner"}; }
-          { command = "floating enable, border none"; criteria = {class = "Kmix"}; }
-          { command = "floating enable, border none"; criteria = {class = "Klipper"}; }
-          { command = "floating enable, border none"; criteria = {class = "Plasmoidviewer"}; }
-          { command = "floating enable, border none, move right 700px, move down 450px"; criteria = {class = "plasmashell"; window_type = "notification"}; }
-          { command = "floating enable, border none, move right 700px, move down 450px"; criteria = {class = "plasmashell"; window_type = "notification"}; }
+          { command = "kill, floating enable, border none"; criteria = {title = "Desktop - Plasma";}; }
+          { command = "floating enable"; criteria = {class = "plasmashell";}; }
+          { command = "floating enable, border none"; criteria = {class = "Plasma";}; }
+          { command = "floating enable, border none"; criteria = {title = "plasma-desktop";}; }
+          { command = "floating enable, border none"; criteria = {title = "win7";}; }
+          { command = "floating enable, border none"; criteria = {class = "krunner";}; }
+          { command = "floating enable, border none"; criteria = {class = "Kmix";}; }
+          { command = "floating enable, border none"; criteria = {class = "Klipper";}; }
+          { command = "floating enable, border none"; criteria = {class = "Plasmoidviewer";}; }
+          { command = "floating enable, border none, move right 700px, move down 450px"; criteria = {class = "plasmashell"; window_type = "notification";}; }
         ];
       };
     };
