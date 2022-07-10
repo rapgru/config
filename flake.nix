45269658867ddbc8804fcc9d9a76e2954a059ca0
Raphael Gruber <raphael.gruber@rapgru.com>
2022-07-10 15:13:31 +0200

    remove sd card image

--- flake.nix
+++ flake.nix
@@ -96,31 +96,7 @@
       };
     };
 
-    images.rpi2 = nixosConfigurations.rpi2.config.system.build.sdImage;
-
     nixosConfigurations = {
-      rpi2 = nixpkgs.lib.nixosSystem {
-        system = "armv7l-linux";
-        modules = [
-          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-raspberrypi.nix"
-          {
-            nixpkgs.config.allowUnsupportedSystem = true;
-            nixpkgs.crossSystem.system = "armv7l-linux";
-            # ... extra configs as above
-            services.openssh = {
-              enable = true;
-              passwordAuthentication = false;
-              permitRootLogin = "yes";
-            };
-
-            users.users."root".openssh.authorizedKeys.keys = [
-              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDqt4kenF3gehfHhTbH1Un9Yc5ClKzHPkuA/7VCkSvq8tI+qrA5JJajuh7MpmGrNVh+aQ88oHOC1fqMJ4XdxJxc3zcvKcaueI78CfqV0MqzSkZADmthlbEonJsh1usgHx+IOxtELPVLw39qTflsHLlAq8gO1mOuQ+pI9TAPnzME6qTafPhkLiTr00S8cW6xcl7dpw0BnGGbEBNiS5ofsbzIYQQfTj1iO/GSBoEMYamJGcsxFdgnqtyZdmctiHjRX/8esOSwL5vbrdklOVJM4ES8U62m5XS3oihnEzBJkKlPWKvvE2cJgdIQGgT5fpl6dQWG8a8jqHU5A7nn2M50sG9mpF0bZupzR6Vg4E8yenEZa2ct06caE4rqlx0CRa3mZexFyqTgS90t3duuyI/NMp8e9lzS5QYWzwBSoYIexA6Q6py5fqE464PeBwsHi6QD8o3+IGPxLME0B8W/lQywHt+YvpHqlm52FqVcjaqrh5Ew3EOYBMrgFbsraChRPdVpWPIVPTVJ6LkwhLkrx33TeO/PnTseNs4AtupKZXDk9jEzC9LYkSHhANTsLo6ZRwXnmjXSjRKAvvQkOZdp2vHg/4IdRVa4OoF2cNHkIVG/sqCKV/HQTppIyqyA8vuwz4Wa2BTLabzjYveI7E+Tw3RF6ocx03FksfwD3Kq+EX8DnCgMWw== cardno:000615587769"
-              # note: ssh-copy-id will add user@clientmachine after the public key
-              # but we can remove the "@clientmachine" part
-            ];
-          }
-        ];
-      };
       "installer-iso" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
